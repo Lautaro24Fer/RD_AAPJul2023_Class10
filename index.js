4294c17efbb0d71ab3624454c66a20a9e77c81d5
Lautaro <LautaroFer456@gmail.com>
2023-08-27 15:53:47 -0300

    Clase 8

---
+++ index.js
@@ -0,0 +1,9 @@
+//Ejercicio 1
+let nombre = prompt("Ingrese un nombre");
+let apellido = prompt("Ingrese un apellido");
+console.log(nombre + apellido);
+
+//Ejercicio 2
+let numero1 = parseFloat(prompt("Ingrese un numero"));
+let numero2 = parseFloat(prompt("Ingrese un segundo numero"));
+console.log(numero1 + numero2);
